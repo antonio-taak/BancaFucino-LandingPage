61bd97ec0e1eab3b7ee0ec1a1303fe2d6df5b3ce
antonio-taak <antonio.riccio@taak.xyz>
2022-05-18 15:27:46 +0200

    modal fix

--- script.js
+++ script.js
@@ -12,112 +12,135 @@ var button_up = document.getElementById("p2-up");
 
 button_down.onclick = function() {
 
-    if(title.className == "pt1 ms-5 pt-4"){
+    if(title.className == "pt1 ms-5 pt-4 fade-in" || title.className == "pt1 ms-5 pt-4 fade-in-inv"){
         //  SE NELLA PARTE 1
         //  VADO ALLA PARTE 2
         setTimeout(()=>{
-            partnum.className = "pt2 titolo me-5";
-            partnum.innerHTML = "2";
+            setTimeout(()=>{
+                partnum.className = "pt2 titolo me-5 fade-in";
+                partnum.innerHTML = "2";
 
-            title.className = "pt2 ms-5 pt-4";
-            title.innerHTML = "Home Banking e Mobile Banking";
+                title.className = "pt2 ms-5 pt-4 fade-in";
+                title.innerHTML = "Home Banking e Mobile Banking";
 
-            subtitle.className = "pt2 fs-5";
-            subtitle.innerHTML = "TANTI SERVIZI INNOVATIVI";
+                subtitle.className = "pt2 fs-5 fade-in";
+                subtitle.innerHTML = "TANTI SERVIZI INNOVATIVI";
 
-            paragraph.className = "pt2";
-            paragraph.innerHTML = "Un design moderno, funzionalità innovative e una navigazione intuitiva e veloce disponibile tramite Computer e App";
+                paragraph.className = "pt2 fade-in";
+                paragraph.innerHTML = "Un design moderno, funzionalità innovative e una navigazione intuitiva e veloce disponibile tramite Computer e App";
 
-            image.src="images/img_p2-2.png";
-            image.className = "p2-img-fade-in";
+                image.src="images/img_p2-2.png";
+                image.className = "p2-img-fade-in";
 
-            button_up.innerHTML = "<img src='/images/slider-up-blue.svg' class='mb-3'> "
-        },700);
+                button_up.innerHTML = "<img src='/images/slider-up-blue.svg' class='mb-3'> "
 
-        //ANIMAZIONE FADE
+            },333);
+            //ANIMAZIONE FADE-IN
+            partnum.className = "titolo col-sm-2 me-5 fading";
+            title.className = "col-sm-5 ms-5 fading";
+            subtitle.className = "fs-5 fading";
+            paragraph.className = "fading";
+        },333);
+
+        //ANIMAZIONE FADE-OUT
         partnum.className = "titolo col-sm-2 me-5 fade-out";
         title.className = "col-sm-5 ms-5 fade-out";
         subtitle.className = "fs-5 fade-out";
         paragraph.className = "fade-out";
         image.className = "p2-img-fade-out";
 
-    }else if(title.className == "pt2 ms-5 pt-4"){
+    }else if(title.className == "pt2 ms-5 pt-4 fade-in" || title.className == "pt2 ms-5 pt-4 fade-in-inv"){
         //  SE NELLA PARTE 2 
         //  VADO ALLA PARTE 3
         setTimeout(()=>{
-            partnum.className = "pt3 titolo me-5";
-            partnum.innerHTML = "3";
+            setTimeout(()=>{
+                partnum.className = "pt3 titolo me-5 fade-in";
+                partnum.innerHTML = "3";
 
-            title.className = "pt3 ms-5 pt-4";
-            title.innerHTML = "Carta di debito PagoBANCOMAT";
+                title.className = "pt3 ms-5 pt-4 fade-in";
+                title.innerHTML = "Carta di debito PagoBANCOMAT";
 
-            subtitle.className = "pt3 fs-5";
-            subtitle.innerHTML = "PER I TUOI PAGAMENTI IN TOTALE SICUREZZA";
+                subtitle.className = "pt3 fs-5 fade-in";
+                subtitle.innerHTML = "PER I TUOI PAGAMENTI IN TOTALE SICUREZZA";
 
-            paragraph.className = "pt3";
-            paragraph.innerHTML = "La tua carta di debito per acquistare prodotti e/o servizi presso i negozi e per prelevare denaro contante presso tutti gli ATM";
+                paragraph.className = "pt3 fade-in";
+                paragraph.innerHTML = "La tua carta di debito per acquistare prodotti e/o servizi presso i negozi e per prelevare denaro contante presso tutti gli ATM";
+            },333);
+            //ANIMAZIONE FADE-IN
+            partnum.className = "titolo col-sm-2 me-5 fading";
+            title.className = "col-sm-5 ms-5 fading";
+            subtitle.className = "fs-5 fading";
+            paragraph.className = "fading";
+        },333);
 
-            //button_up.innerHTML = "<img src='/images/slider-up-blue.svg' class='mb-3'> "
-        },700);
-
-        //ANIMAZIONE FADE
+        //ANIMAZIONE FADE-OUT
         partnum.className = "titolo col-sm-2 me-5 fade-out";
         title.className = "col-sm-5 ms-5 fade-out";
         subtitle.className = "fs-5 fade-out";
         paragraph.className = "fade-out";
 
-    }else if(title.className == "pt3 ms-5 pt-4"){
+    }else if(title.className == "pt3 ms-5 pt-4 fade-in" || title.className == "pt3 ms-5 pt-4 fade-in-inv"){
         //  SE NELLA PARTE 3
         //  VADO ALLA PARTE 4
         setTimeout(()=>{
-            partnum.className = "pt4 titolo me-5";
-            partnum.innerHTML = "4";
+            setTimeout(()=>{
+                partnum.className = "pt4 titolo me-5 fade-in";
+                partnum.innerHTML = "4";
 
-            title.className = "pt4 ms-5 pt-4";
-            title.innerHTML = "SMS Alert";
+                title.className = "pt4 ms-5 pt-4 fade-in";
+                title.innerHTML = "SMS Alert";
 
-            subtitle.className = "pt4 fs-5";
-            subtitle.innerHTML = "I TUOI MOVIMENTI SOTTO CONTROLLO";
+                subtitle.className = "pt4 fs-5 fade-in";
+                subtitle.innerHTML = "I TUOI MOVIMENTI SOTTO CONTROLLO";
 
-            paragraph.className = "pt4";
-            paragraph.innerHTML = "Ad ogni movimento sul tuo conto riceverai un SMS sul tuo cellulare. La tua sicurezza ci sta a cuore!";
+                paragraph.className = "pt4 fade-in";
+                paragraph.innerHTML = "Ad ogni movimento sul tuo conto riceverai un SMS sul tuo cellulare. La tua sicurezza ci sta a cuore!";
+            },333);
+            //ANIMAZIONE FADE-IN
+            partnum.className = "titolo col-sm-2 me-5 fading";
+            title.className = "col-sm-5 ms-5 fading";
+            subtitle.className = "fs-5 fading";
+            paragraph.className = "fading";
+        },333);
 
-            //button_up.innerHTML = "<img src='/images/slider-up-blue.svg' class='mb-3'> "
-        },700);
-
-        //ANIMAZIONE FADE
+        //ANIMAZIONE FADE-OUT
         partnum.className = "titolo col-sm-2 me-5 fade-out";
         title.className = "col-sm-5 ms-5 fade-out";
         subtitle.className = "fs-5 fade-out";
         paragraph.className = "fade-out";
 
-    }else if(title.className == "pt4 ms-5 pt-4"){
+    }else if(title.className == "pt4 ms-5 pt-4 fade-in" || title.className == "pt4 ms-5 pt-4 fade-in-inv"){
         //  SE NELLA PARTE 4
         //  VADDO ALLA PARTE 5
         setTimeout(()=>{
-            partnum.className = "pt5 titolo me-5";
-            partnum.innerHTML = "5";
+            setTimeout(()=>{
+                partnum.className = "pt5 titolo me-5 fade-in";
+                partnum.innerHTML = "5";
 
-            title.className = "pt5 ms-5 pt-4";
-            title.innerHTML = "PFM (Personal Financial Manager)";
+                title.className = "pt5 ms-5 pt-4 fade-in";
+                title.innerHTML = "PFM (Personal Financial Manager)";
 
-            subtitle.className = "pt5 fs-5";
-            subtitle.innerHTML = "UN NUOVO MODO DI GESTIRE LE TUE OPERAZIONI";
+                subtitle.className = "pt5 fs-5 fade-in";
+                subtitle.innerHTML = "UN NUOVO MODO DI GESTIRE LE TUE OPERAZIONI";
 
-            paragraph.className = "pt5";
-            paragraph.innerHTML = "Con il PFM hai a disposizione un Personal Trainer Finanziario che ti aiuta con consigli mirati per la gestione delle tue entrare ed uscite";
+                paragraph.className = "pt5 fade-in";
+                paragraph.innerHTML = "Con il PFM hai a disposizione un Personal Trainer Finanziario che ti aiuta con consigli mirati per la gestione delle tue entrare ed uscite";
 
-            button_down.innerHTML = "<img src='/images/slider-down.svg' class='mb-3'> "
-        },700);
+                button_down.innerHTML = "<img src='/images/slider-down.svg' class='mb-3'> "
+            },333);
+            //ANIMAZIONE FADE-IN
+            partnum.className = "titolo col-sm-2 me-5 fading";
+            title.className = "col-sm-5 ms-5 fading";
+            subtitle.className = "fs-5 fading";
+            paragraph.className = "fading";
+        },333);
 
-        //ANIMAZIONE FADE
+        //ANIMAZIONE FADE-OUT
         partnum.className = "titolo col-sm-2 me-5 fade-out";
         title.className = "col-sm-5 ms-5 fade-out";
         subtitle.className = "fs-5 fade-out";
         paragraph.className = "fade-out";
 
-    }else{
-        
     }
 }
 
@@ -127,108 +150,134 @@ button_down.onclick = function() {
 
 button_up.onclick = function() {
 
-    if(title.className == "pt5 ms-5 pt-4"){
+    if(title.className == "pt5 ms-5 pt-4 fade-in" || title.className == "pt5 ms-5 pt-4 fade-in-inv"){
         //  SONO NELLA PARTE 5
         //  VADO ALLA PARTE 4
         setTimeout(()=>{
-            partnum.className = "pt4 titolo me-5";
-            partnum.innerHTML = "4";
+            setTimeout(()=>{
+                partnum.className = "pt4 titolo me-5 fade-in-inv";
+                partnum.innerHTML = "4";
 
-            title.className = "pt4 ms-5 pt-4";
-            title.innerHTML = "SMS Alert";
+                title.className = "pt4 ms-5 pt-4 fade-in-inv";
+                title.innerHTML = "SMS Alert";
 
-            subtitle.className = "pt4 fs-5";
-            subtitle.innerHTML = "I TUOI MOVIMENTI SOTTO CONTROLLO";
+                subtitle.className = "pt4 fs-5 fade-in-inv";
+                subtitle.innerHTML = "I TUOI MOVIMENTI SOTTO CONTROLLO";
 
-            paragraph.className = "pt4";
-            paragraph.innerHTML = "Ad ogni movimento sul tuo conto riceverai un SMS sul tuo cellulare. La tua sicurezza ci sta a cuore!";
+                paragraph.className = "pt4 fade-in-inv";
+                paragraph.innerHTML = "Ad ogni movimento sul tuo conto riceverai un SMS sul tuo cellulare. La tua sicurezza ci sta a cuore!";
 
-            button_down.innerHTML = "<img src='/images/slider-down-blue.svg' class='mb-3'> "
-        },700);
+                button_down.innerHTML = "<img src='/images/slider-down-blue.svg' class='mb-3'> "
+            },333);
+            //ANIMAZIONE FADE-IN
+            partnum.className = "titolo col-sm-2 me-5 fading";
+            title.className = "col-sm-5 ms-5 fading";
+            subtitle.className = "fs-5 fading";
+            paragraph.className = "fading";
+        },333);
 
-        //ANIMAZIONE FADE
-        partnum.className = "titolo col-sm-2 me-5 fade-out";
-        title.className = "col-sm-5 ms-5 fade-out";
-        subtitle.className = "fs-5 fade-out";
-        paragraph.className = "fade-out";
+        //ANIMAZIONE FADE-OUT
+        partnum.className = "titolo col-sm-2 me-5 fade-out-inv";
+        title.className = "col-sm-5 ms-5 fade-out-inv";
+        subtitle.className = "fs-5 fade-out-inv";
+        paragraph.className = "fade-out-inv";
 
-    }else if(title.className == "pt4 ms-5 pt-4"){
+    }else if(title.className == "pt4 ms-5 pt-4 fade-in" || title.className == "pt4 ms-5 pt-4 fade-in-inv"){
         //  SONO NELLA PARTE 4
         //  VADO ALLA PARTE 3
         setTimeout(()=>{
-            partnum.className = "pt3 titolo me-5";
-            partnum.innerHTML = "3";
+            setTimeout(()=>{
+                partnum.className = "pt3 titolo me-5 fade-in-inv";
+                partnum.innerHTML = "3";
 
-            title.className = "pt3 ms-5 pt-4";
-            title.innerHTML = "Carta di debito PagoBANCOMAT";
+                title.className = "pt3 ms-5 pt-4 fade-in-inv";
+                title.innerHTML = "Carta di debito PagoBANCOMAT";
 
-            subtitle.className = "pt3 fs-5";
-            subtitle.innerHTML = "PER I TUOI PAGAMENTI IN TOTALE SICUREZZA";
+                subtitle.className = "pt3 fs-5 fade-in-inv";
+                subtitle.innerHTML = "PER I TUOI PAGAMENTI IN TOTALE SICUREZZA";
 
-            paragraph.className = "pt3";
-            paragraph.innerHTML = "La tua carta di debito per acquistare prodotti e/o servizi presso i negozi e per prelevare denaro contante presso tutti gli ATM";
+                paragraph.className = "pt3 fade-in-inv";
+                paragraph.innerHTML = "La tua carta di debito per acquistare prodotti e/o servizi presso i negozi e per prelevare denaro contante presso tutti gli ATM";
+            },333);
+            //ANIMAZIONE FADE-IN
+            partnum.className = "titolo col-sm-2 me-5 fading";
+            title.className = "col-sm-5 ms-5 fading";
+            subtitle.className = "fs-5 fading";
+            paragraph.className = "fading";
+        },333);
 
-            //button_up.innerHTML = "<img src='/images/slider-up-blue.svg' class='mb-3'> "
-        },700);
+        //ANIMAZIONE FADE-OUT
+        partnum.className = "titolo col-sm-2 me-5 fade-out-inv";
+        title.className = "col-sm-5 ms-5 fade-out-inv";
+        subtitle.className = "fs-5 fade-out-inv";
+        paragraph.className = "fade-out-inv";
 
-        //ANIMAZIONE FADE
-        partnum.className = "titolo col-sm-2 me-5 fade-out";
-        title.className = "col-sm-5 ms-5 fade-out";
-        subtitle.className = "fs-5 fade-out";
-        paragraph.className = "fade-out";
-
-    }else if(title.className == "pt3 ms-5 pt-4"){
+    }else if(title.className == "pt3 ms-5 pt-4 fade-in" || title.className == "pt3 ms-5 pt-4 fade-in-inv"){
         //  SONO NELLA PARTE 3
         //  VADO ALLA PARTE 2
         setTimeout(()=>{
-            partnum.className = "pt2 titolo me-5";
-            partnum.innerHTML = "2";
+            setTimeout(()=>{
+                partnum.className = "pt2 titolo me-5 fade-in-inv";
+                partnum.innerHTML = "2";
 
-            title.className = "pt2 ms-5 pt-4";
-            title.innerHTML = "Home Banking e Mobile Banking";
+                title.className = "pt2 ms-5 pt-4 fade-in-inv";
+                title.innerHTML = "Home Banking e Mobile Banking";
 
-            subtitle.className = "pt2 fs-5";
-            subtitle.innerHTML = "TANTI SERVIZI INNOVATIVI";
+                subtitle.className = "pt2 fs-5 fade-in-inv";
+                subtitle.innerHTML = "TANTI SERVIZI INNOVATIVI";
 
-            paragraph.className = "pt2";
-            paragraph.innerHTML = "Un design moderno, funzionalità innovative e una navigazione intuitiva e veloce disponibile tramite Computer e App";
+                paragraph.className = "pt2 fade-in-inv";
+                paragraph.innerHTML = "Un design moderno, funzionalità innovative e una navigazione intuitiva e veloce disponibile tramite Computer e App";
 
-            button_up.innerHTML = "<img src='/images/slider-up-blue.svg' class='mb-3'> "
-        },700);
+                button_up.innerHTML = "<img src='/images/slider-up-blue.svg' class='mb-3'> "
+            },333);
+            //ANIMAZIONE FADE-IN
+            partnum.className = "titolo col-sm-2 me-5 fading";
+            title.className = "col-sm-5 ms-5 fading";
+            subtitle.className = "fs-5 fading";
+            paragraph.className = "fading";
+        },333);
 
-        //ANIMAZIONE FADE
-        partnum.className = "titolo col-sm-2 me-5 fade-out";
-        title.className = "col-sm-5 ms-5 fade-out";
-        subtitle.className = "fs-5 fade-out";
-        paragraph.className = "fade-out";
+        //ANIMAZIONE FADE-OUT
+        partnum.className = "titolo col-sm-2 me-5 fade-out-inv";
+        title.className = "col-sm-5 ms-5 fade-out-inv";
+        subtitle.className = "fs-5 fade-out-inv";
+        paragraph.className = "fade-out-inv";
 
-    }else if(title.className == "pt2 ms-5 pt-4"){
+    }else if(title.className == "pt2 ms-5 pt-4 fade-in" || title.className == "pt2 ms-5 pt-4 fade-in-inv"){
         //  SONO NELLA PARTE 2
         //  VADO ALLA PARTE 1
         setTimeout(()=>{
-            partnum.className = "pt1 titolo me-5";
-             partnum.innerHTML = "1";
+            setTimeout(()=>{
+                partnum.className = "pt1 titolo me-5 fade-in-inv";
+                partnum.innerHTML = "1";
 
-             title.className = "pt1 ms-5 pt-4";
-             title.innerHTML = "Conto Corrente";
+                title.className = "pt1 ms-5 pt-4 fade-in-inv";
+                title.innerHTML = "Conto Corrente";
 
-             subtitle.className = "pt1 fs-5";
-             subtitle.innerHTML = "TUTTO A PORTATA DI CLICK";
+                subtitle.className = "pt1 fs-5 fade-in-inv";
+                subtitle.innerHTML = "TUTTO A PORTATA DI CLICK";
 
-             paragraph.className = "pt1";
-             paragraph.innerHTML = "Con conto [nome] gestire i tuoi risparmi e sempre più facile e veloce";
+                paragraph.className = "pt1 fade-in-inv";
+                paragraph.innerHTML = "Con conto [nome] gestire i tuoi risparmi e sempre più facile e veloce";
 
-             image.src = "images/img_p2.png";
-             image.className = "p2-img-fade-in";
+                image.src = "images/img_p2.png";
+                image.className = "p2-img-fade-in";
 
-             button_up.innerHTML = "<img src='/images/slider-up.svg' class='mb-3'> "
-         },700);
+                button_up.innerHTML = "<img src='/images/slider-up.svg' class='mb-3'> "
+            },333);
+             //ANIMAZIONE FADE-IN
+            partnum.className = "titolo col-sm-2 me-5 fading";
+            title.className = "col-sm-5 ms-5 fading";
+            subtitle.className = "fs-5 fading";
+            paragraph.className = "fading";
+         },333);
 
-        //ANIMAZIONE FADE
-        partnum.className = "p2 titolo col-sm-2 me-5 fade-out";
-        title.className = "p2 col-sm-5 ms-5 fade-out";
-        subtitle.className = "p2 fs-5 fade-out";
-        paragraph.className = "p2 fade-out";
+        //ANIMAZIONE FADE-OUT
+        partnum.className = "p2 titolo col-sm-2 me-5 fade-out-inv";
+        title.className = "p2 col-sm-5 ms-5 fade-out-inv";
+        subtitle.className = "p2 fs-5 fade-out-inv";
+        paragraph.className = "p2 fade-out-inv";
         image.className = "p2-img-fade-out"
     }
 
